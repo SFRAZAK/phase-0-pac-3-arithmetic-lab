15c6426f8713e4648d25fac070158ac76c7c791a
Shaun Razak <shaun.f.razak@gmail.com>
2023-01-08 19:15:40 -0500

    commit tree ab7f93ee5f75827922a5e97b36eabf07f74c2aa9

---
+++ index.js
@@ -0,0 +1,55 @@
+function add() {
+
+}
+function subtract() {
+
+}
+function multiply() {
+
+}
+function divide() {
+
+}
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function add(number) {
+    return number === a + 1;
+}
+function add(a, b) {
+    return a + b;
+}
+function increment(number) {
+    return (number + 1);
+}
+function decrement(number) {
+    return (number - 1);
+}
+function makeInt(number) {
+    return number;
+}
+
+//function makeInt() {
+  //  makeInt();
+    //return 0;
+//}
+
+//function preserveDecimal(string) {
+  //  return string;
+//}
+function preserveDecimal(number) {
+    return parseFloat(number);
+}
+function parseInt() {
+    parseInt("2.222");
+    return number(2.222);
+}
